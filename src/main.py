23b4aa9b58b8a5233e6a43f04aeb328ba11efc68
Nicholas Omer Chiasson <nicholaschiasson@namnamnamnam.com>
2016-12-07 02:56:35 -0500

    Add plugboard support. Requires testing. Takes 30+ minutes to decrypt

--- src/main.py
+++ src/main.py
@@ -11,6 +11,19 @@ Quadgram  = ns("english_fitness_statistics/english_quadgrams.txt")
 
 Rotors    = {1: Rotor.I, 2: Rotor.II, 3: Rotor.III, 4: Rotor.IV, 5: Rotor.V}
 
+def UniqueBigrams():
+    Bigrams = {}
+    for l in range(ord("A"), ord("Z") + 1):
+        c = l - 65
+        lchr = chr(l)
+        remainingAlphabet = range(ord("A"), ord("Z") + 1)[:c] + range(ord("A"), ord("Z") + 1)[c+1:]
+        for r in remainingAlphabet:
+            rchr = chr(r)
+            if lchr not in Bigrams:
+                Bigrams[lchr] = []
+            Bigrams[lchr].append(rchr)
+    return Bigrams
+
 class EnigmaBreaker:
     def __init__(self, cipher):
         self.best_score                 = float("-inf")
@@ -18,6 +31,7 @@ class EnigmaBreaker:
         self.best_ringstellung          = "AAA"
         self.best_steckerverbindungen   = ""
         self.best_indicator             = "AAA"
+        self.ngram                      = Quadgram
         self.cipher                     = cipher
 
     def new_enigma(self, walzenlage=None, ringstellung=None, steckerverbindungen=None, indicator=None):
@@ -41,7 +55,7 @@ class EnigmaBreaker:
                     for leftInd in range(ord("A"), ord("Z") + 1):
                         for middleInd in range(ord("A"), ord("Z") + 1):
                             for rightInd in range(ord("A"), ord("Z") + 1):
-                                score = Quadgram.score(self.new_enigma(walzenlage=(lRot, mRot, rRot), indicator=(chr(leftInd) + chr(middleInd) + chr(rightInd))).run(self.cipher))
+                                score = self.ngram.score(self.new_enigma(walzenlage=(lRot, mRot, rRot), indicator=(chr(leftInd) + chr(middleInd) + chr(rightInd))).run(self.cipher))
                                 if score > self.best_score:
                                     self.best_score = score
                                     self.best_walzenlage = [leftRot, middleRot, rightRot]
@@ -58,23 +72,71 @@ class EnigmaBreaker:
                 for rightRing in range(ord("A"), ord("Z") + 1):
                     ring = "A" + chr(middleRing) + chr(rightRing)
                     indi = self.best_indicator[0] + chr((((ord(midInd) - 65) + (middleRing - 65)) % 26) + 65) + chr((((ord(rightInd) - 65) + (rightRing - 65)) % 26) + 65)
-                    score = Quadgram.score(self.new_enigma(ringstellung=ring, indicator=indi).run(self.cipher))
+                    score = self.ngram.score(self.new_enigma(ringstellung=ring, indicator=indi).run(self.cipher))
                     if score > self.best_score:
                         self.best_score = score
                         self.best_ringstellung = ring
                         self.best_indicator = indi
 
+    def break_plugboard(self):
+        self.break_ringstellung()
+        bgrams = UniqueBigrams()
+        keepTrying = True
+        while keepTrying:
+            keepTrying = False
+            stecCandidate = ""
+            for c in range(ord("A"), ord("Z") + 1):
+                l = chr(c)
+                if l in bgrams:
+                    for r in bgrams[l]:
+                        if r in bgrams:
+                            stec = (self.best_steckerverbindungen + " " + l + r).strip()
+                            score = self.ngram.score(self.new_enigma(steckerverbindungen=stec).run(self.cipher))
+                            if score > self.best_score:
+                                keepTrying = True
+                                self.best_score = score
+                                stecCandidate = l + r
+            if (keepTrying):
+                self.best_steckerverbindungen = (self.best_steckerverbindungen + " " + stecCandidate).strip()
+                del bgrams[stecCandidate[0]]
+                del bgrams[stecCandidate[1]]
+
+    def break_full(self):
+        self.break_plugboard()
+
 # Approximately 11 minutes to decipher this.
-cipherText = "NPNKANVHWKPXORCDDTRJRXSJFLCIUAIIBUNQIUQFTHLOZOIMENDNGPCB"
-eb = EnigmaBreaker(cipherText)
+#cipherText = "NPNKANVHWKPXORCDDTRJRXSJFLCIUAIIBUNQIUQFTHLOZOIMENDNGPCB"
+#eb = EnigmaBreaker(cipherText)
+#start = time.clock()
+#eb.break_full()
+#end = time.clock()
+#ne = eb.new_enigma()
+#print "Cipher Text:        ", cipherText
+#print "Walzenlage:         ", ne.rotors[0].name,  ne.rotors[1].name,  ne.rotors[2].name
+#print "Indicator:          ", ne.rotors[0].shift, ne.rotors[1].shift, ne.rotors[2].shift
+#print "Ringstellung:       ", ne.rotors[0].ring,  ne.rotors[1].ring,  ne.rotors[2].ring
+#print "Steckerverbindungen:", eb.best_steckerverbindungen
+#print "Plain Text:         ", ne.run(cipherText)
+#print "Score:              ", eb.best_score
+#print "Time:               ", (end - start)
+
+longCipherText          = "CTSXBWMFIKEZWCOXYNFYOHAJAWGGBFJXBAYAWYNXCUUYYLGVSKDLNDHBKTFBLQPHGXUFKKWTCFNLDEJPKNQQFDMRRLNZVZQNXVLLWAVWQTUIYXVORZERRPMNAWJTPYVUYMIYIYHCSIZFQCMMVDWGAADBHOSJOPAVQWXEFAEMCXBQQUIVNJRUZAVQJDNRYHAKJMHVUJQFAAWYFQHKSFUYPPGFCDXIQVGFUZTFMLZKAPOPIHEOOVKKTMACIJNICFJFQQHJEUDBEJVNIFTYRSRCQVPMOMDIILFIALJZJEAFFDVHOPJKLWJFTVUHMQWUKYCNJXMHZQJVFGOGFQEVPDTQIIZURETCFKDJQTXPRUSGKWVNPPVHWQIXFFUDPHXWNKMDLULZEIODGRQARSIXPUTEUDIQEKXUQGJCIKFOSBLYJIYUBZHYRJQRGIMRMVRNLSHGBRWCBEDTDGEZPJFDDRKAYVJPOLSHWNMXUKYKVFRFTMYSOCTZOAEPKBYQIRAWABHRDUYMWJRXQWMRTIWNNBWUNRJJBZWIBWWGTSXYIAEHHWQXIJUJMIYZRWADCTJUQLWYDZOPHQIE"
+longPlainText           = "HOWDOYOUTHINKIAMGOINGTOGETALONGWITHOUTYOUWHENYOUAREGONEYOUTOOKMEFOREVERYTHINGTHATIHADANDKICKEDMEOUTONMYOWNAREYOUHAPPYAREYOUSATISFIEDHOWLONGCANYOUSTANDTHEHEATOUTOFTHEDOORWAYTHEBULLETSRIPTOTHESOUNDOFTHEBEATANOTHERONEBITESTHEDUSTANOTHERONEBITESTHEDUSTANOTHERONEBITESTHEDUSTANOTHERONEBITESTHEDUSTHOWDOYOUTHINKIAMGOINGTOGETALONGWITHOUTYOUWHENYOUAREGONEYOUTOOKMEFOREVERYTHINGTHATIHADANDKICKEDMEOUTONMYOWNAREYOUHAPPYAREYOUSATISFIEDHOWLONGCANYOUSTANDTHEHEATOUTOFTHEDOORWAYTHEBULLETSRIPTOTHESOUNDOFTHEBEATANOTHERONEBITESTHEDUSTANOTHERONEBITESTHEDUSTANOTHERONEBITESTHEDUSTANOTHERONEBITESTHEDUST"
+walzenlageUsed          = (Rotor.III(), Rotor.V(), Rotor.I())
+ringstellungUsed        = "WFP"
+steckerverbindungenUsed = "AC EI FW GM JT LP QY"
+indicatorUsed           = "FPM"
+
+eb = EnigmaBreaker(longCipherText)
 start = time.clock()
-eb.break_ringstellung()
+eb.break_full()
 end = time.clock()
 ne = eb.new_enigma()
-print "Cipher Text: ", cipherText
-print "Walzenlage:  ", ne.rotors[0].name,  ne.rotors[1].name,  ne.rotors[2].name
-print "Indicator:   ", ne.rotors[0].shift, ne.rotors[1].shift, ne.rotors[2].shift
-print "Ringstellung:", ne.rotors[0].ring,  ne.rotors[1].ring,  ne.rotors[2].ring
-print "Plain Text:  ", ne.run(cipherText)
-print "Score:       ", eb.best_score
-print "Time:        ", (end - start)
+print "Cipher Text:        ", longCipherText
+print "Walzenlage:         ", ne.rotors[0].name,  ne.rotors[1].name,  ne.rotors[2].name
+print "Indicator:          ", ne.rotors[0].shift, ne.rotors[1].shift, ne.rotors[2].shift
+print "Ringstellung:       ", ne.rotors[0].ring,  ne.rotors[1].ring,  ne.rotors[2].ring
+print "Steckerverbindungen:", eb.best_steckerverbindungen
+print "Plain Text:         ", ne.run(longCipherText)
+print "Score:              ", eb.best_score
+print "Time:               ", (end - start)
